1e06cd11adf5a8a10f878cc21a505f053ed50dfb
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-07-24 22:17:54 +0200

    Python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name='pursue',
+    version='0.1.0',
+    description='OpenStack Object Storage Python Client',
+    long_description=long_description,
+    author='Jaime Gil de Sagredo Luna',
+    author_email='jaimegildesagredo@gmail.com',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
